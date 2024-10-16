f65c581165ff7e30c23cf44553802a0349d5568c
Girish Adhikari <adhgirish@gmail.com>
2024-10-16 19:26:38 +0545

    created registration, login and logout

--- fasalmela/base/views.py
+++ fasalmela/base/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .forms import SignUpForm, ItemListForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'login.html')
+    else:
+        form = SignUpForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'signup.html', context)
+
+def addItem(request):
+    if request.method == 'POST':
+        form = ItemListForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'home.html')
+    else:
+        form = ItemListForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'addItem.html', context)
